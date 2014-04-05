a88bd0be43b2d0f19786270dbde0a7721868da43
Mimi Flynn <mimiflynn@gmail.com>
2014-04-05 18:56:42 -0400

    remove login req

--- app/routes/rideshare.js
+++ app/routes/rideshare.js
@@ -2,23 +2,14 @@
 
 // rideshares routes use rideshare controller
 var rideshares = require('../controllers/rideshare');
-var authorization = require('./middlewares/authorization');
-
-// rideshare authorization helpers
-var hasAuthorization = function(req, res, next) {
-	if (req.rideshare.user.id !== req.user.id) {
-        return res.send(401, 'User is not authorized');
-    }
-    next();
-};
 
 module.exports = function(app) {
 
     app.get('/rideshare', rideshares.all);
-    app.post('/rideshare', authorization.requiresLogin, rideshares.create);
+    app.post('/rideshare', rideshares.create);
     app.get('/rideshare/:rideshareId', rideshares.show);
-    app.put('/rideshare/:rideshareId', authorization.requiresLogin, hasAuthorization, rideshares.update);
-    app.del('/rideshare/:rideshareId', authorization.requiresLogin, hasAuthorization, rideshares.destroy);
+    app.put('/rideshare/:rideshareId', rideshares.update);
+    app.del('/rideshare/:rideshareId', rideshares.destroy);
 
     // Finish with setting up the rideshareId param
     app.param('rideshareId', rideshares.rideshare);
