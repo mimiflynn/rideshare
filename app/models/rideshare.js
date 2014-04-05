3b394dbc7145d6c1f10a3561350f40db5b18e021
Mimi Flynn <mimiflynn@gmail.com>
2014-04-05 17:35:18 -0400

    rideshare schema created and registered

---
+++ app/models/rideshare.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var RideshareSchema = new Schema({
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    name: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    partyNumber: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    arrivalDate: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    arrivalTime: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    arrivalLocation: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    arrivalNotes: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    arrivalEmail: {
+        type: String,
+        default: '',
+        trim: true
+    }
+});
+
+/**
+ * Statics
+ */
+RideshareSchema.statics.load = function(id, cb) {
+    this.findOne({
+        _id: id
+    }).populate('user', 'name username').exec(cb);
+};
+
+mongoose.model('rideshare', RideshareSchema);
--- public/js/rideshare/services.js
+++ public/js/rideshare/services.js
@@ -2,12 +2,12 @@
 
 //Articles service used for articles REST endpoint
 angular.module('rideshare.services', []).
-	factory('Rideshare', ['$resource', function($resource) {
-    	return $resource('rideshare/:rideshareId', {
-        		rideshareId: '@_id'
-    		}, {
-        		update: {
-            		method: 'PUT'
-        		}
-    		});
-	}]);
+    factory('Rideshare', ['$resource', function ($resource) {
+        return $resource('rideshare/:rideshareId', {
+            rideshareId: '@_id'
+        }, {
+            update: {
+                method: 'PUT'
+            }
+        });
+    }]);
