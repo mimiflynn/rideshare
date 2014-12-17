7c1612cfa6a22902d907b835e9f7ef86268997da
Mimi Flynn <mimiflynn@gmail.com>
2014-12-17 11:36:07 -0500

    server setup

--- packages/custom/rideshare/server/routes/rideshare.js
+++ packages/custom/rideshare/server/routes/rideshare.js
@@ -1,26 +1,28 @@
 'use strict';
 
-// The Package is past automatically as first parameter
-module.exports = function(Rideshare, app, auth, database) {
+// rideshares routes use rideshare controller
+var rideshares = require('../controllers/rideshare');
 
-  app.get('/rideshare/example/anyone', function(req, res, next) {
-    res.send('Anyone can access this');
-  });
+// Article authorization helpers
+var hasAuthorization = function(req, res, next) {
+  if (!req.user.isAdmin && req.article.user.id !== req.user.id) {
+    return res.status(401).send('User is not authorized');
+  }
+  next();
+};
 
-  app.get('/rideshare/example/auth', auth.requiresLogin, function(req, res, next) {
-    res.send('Only authenticated users can access this');
-  });
+module.exports = function(Reideshares, app, auth) {
 
-  app.get('/rideshare/example/admin', auth.requiresAdmin, function(req, res, next) {
-    res.send('Only users with Admin role can access this');
-  });
+    app.route('/rideshare')
+      .get(rideshares.all)
+      .post(auth.requiresLogin, rideshares.create);
 
-  app.get('/rideshare/example/render', function(req, res, next) {
-    Rideshare.render('index', {
-      package: 'rideshare'
-    }, function(err, html) {
-      //Rendering a view from the Package server/views
-      res.send(html);
-    });
-  });
+    app.route('/rideshare/:rideshareId')
+      .get(auth.isMongoId, auth.requiresLogin, rideshares.show)
+      .put(auth.isMongoId, auth.requiresLogin, rideshares.update)
+      .delete(auth.isMongoId, auth.requiresLogin, rideshares.destroy);
+
+    // Finish with setting up the rideshareId param
+    app.param('rideshareId', rideshares.rideshare);
+
 };
